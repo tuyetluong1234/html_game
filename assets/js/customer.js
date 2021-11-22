9c21860d9d1ddb069adee65874f75bb23e336d38
-tuyetluongxd <51015826+tuyetluong1234@users.noreply.github.com>
2021-11-23 00:49:50 +0700

    update

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -1,385 +1,88 @@
-
-$(document).ready(function() {
-  // $.sidebarMenu($('.sidebar-menu'))
-
-  /// show popup
-  $(".bus-button-confirm").on("click", function(event){
-      $('#popupConfirmDelete').modal('show');
-  });
-
-  $(".open-find-id").on("click", function(event){
-      $('#popupfind-id').modal('show');
-  });
-
-  $(".open-postcode-member").on("click", function(event){
-      $('#popuppostcode-member').modal('show');
-  });
-  // end show popup
-
-  // add height
-   
-
-
-  // jquery page mamber
-  if ($('.selectpickermember').length) {
-    $('.selectpickermember').selectpicker({
-      style: 'btn-info',
-      size: 4,
-      minDate : moment()
-    });
-  }
-
-  // input radio check
-  $('.member-sub input.radio1:radio').change(function () {
-    if ($(this).is(":checked")) {
-        $(".member-sub").addClass('show-item1');
-        $(".member-sub").removeClass('show-item2');
-    }
-    else {
-        
-    };
-  });
-  $('.member-sub input.radio2:radio').change(function () {
-    if ($(this).is(":checked")) {
-        $(".member-sub").addClass('show-item2');
-        $(".member-sub").removeClass('show-item1');
-    }
-    else {
-        
-    };
-  });
-
-  // input radio bussiness sub detail
-  $('.check-itemselect input.radio1:radio').change(function () {
-    if ($(this).is(":checked")) {
-        $(".show-radiobusiness-detail").addClass('show-item1');
-        $(".show-radiobusiness-detail").removeClass('show-item2');
-    }
-    else {
-        
-    };
-  });
-  $('.check-itemselect input.radio2:radio').change(function () {
-    if ($(this).is(":checked")) {
-        $(".show-radiobusiness-detail").addClass('show-item2');
-        $(".show-radiobusiness-detail").removeClass('show-item1');
-    }
-    else {
-        
-    };
-  });
-
-  // input radio request list
-  $('.radio-check-request-list input.radio1:radio').change(function () {
-    if ($(this).is(":checked")) {
-     $("#buttonSubmitRequestCounselor").removeClass("disabled")
-        $(".request-list-radio1").addClass('show-radio1');
-        $(".request-list-radio2").removeClass('show-radio2');
-    }
-    else {
-        
-    };
-  });
-  $('.radio-check-request-list input.radio2:radio').change(function () {
-    if ($(this).is(":checked")) {
-     $("#buttonSubmitRequestCounselor").removeClass("disabled")
-        $(".request-list-radio1").removeClass('show-radio1');
-        $(".request-list-radio2").addClass('show-radio2');
-    }
-    else {
-        
-    };
-  });
-
-  $(document.body).on('click', '.approval-request-detail .form-footer .v1', function(e) {
-      $(".form-footer1.style1").addClass('show');
-      $(".approval-request-detail .form-footer .v1").addClass('show-before red');
-      $(".form-footer1.style2").removeClass('show');
-      $(".approval-request-detail .form-footer .v2").removeClass('show-before red');
-  });
-  $(document.body).on('click', '.approval-request-detail .form-footer .v2', function(e) {
-      $(".form-footer1.style2").addClass('show');
-      $(".approval-request-detail .form-footer .v2").addClass('show-before red');
-      $(".form-footer1.style1").removeClass('show');
-      $(".approval-request-detail .form-footer .v1").removeClass('show-before red');
-  });
-
-  (function($) {
-    function inputuploadimg() {
-        if (!$('.tr-file-upload.image-upload').length) return;
-
-        $('.tr-file-upload.image-upload').each(function() {
-          var fileUpload = $(this);
-          var showFileInfo = fileUpload.find('.show-file-info');
-          var inputFile = fileUpload.find('.input-file');
-          var previewImage = fileUpload.find('.preview-image > img');
-          var previewImageBg = fileUpload.find('.preview-image > .image-bg');
-
-          previewImage.attr('src') == '' ? previewImage.closest('.preview-image').addClass('no-image') : previewImage.closest('.preview-image').removeClass('no-image');
-          previewImageBg.attr('style') == undefined ? previewImageBg.closest('.preview-image').addClass('no-image') : previewImageBg.closest('.preview-image').removeClass('no-image');
-          
-          var readURL = function(input) {
-            if (input.files && input.files[0]) {
-              var reader = new FileReader();
-
-              reader.onload = function(e) {
-                previewImage.attr('src', e.target.result);
-                previewImageBg.attr('style', 'background-image: url("' + e.target.result + '");');
-              }
-
-              reader.readAsDataURL(input.files[0]);
-            }
-          };
-          inputFile.on('change', function() {
-            var thisInputFile = $(this);
-            var file = thisInputFile[0].files[0];
-            var fileName = file.name;
-            // alert(fileName);
-            showFileInfo.empty().text(fileName);
-          });
-
-          fileUpload.find('.input-file').on('change', function() {
-            readURL(this);
-          });
+(function($) {
+    $("body").imagesLoaded( function() {
+        $("body").addClass("loading-done");
+        $("[data-animate]").scrolla({
+            mobile: true
         });
-    }
-    function addcolum() {
-      
-      $(".add-field").click(function(){
-        // $(this).closest(".program-detail div.md-ct .item-img-section").empty();
-        $(".coun-field .link-dowload").addClass('show-button');
-        $(".coun-field .add-item-field").append('<tr class="add-select"><td><input type="text" name="name" data-required="1" class="form-control"/></td><td><div class="inputfile busine-detail"><div class="tr-file-upload image-upload"><label class="btn green btn-select-file">찾아보기<input type="file" class="hidden input-file"></label><div class="preview-image"><img src="../../assets/images/demo_single_2.jpg" alt="thumbnail"></div></div></div></td></td><td><div class="inputfile busine-detail"><div class="tr-file-upload image-upload"><label class="btn green btn-select-file">찾아보기<input type="file" class="hidden input-file"></label><div class="preview-image"><img src="../../assets/images/demo_single_2.jpg" alt="thumbnail"></div></div></div></td><td><select name="selecttext" class="form-control selectpickermember selectpickeraddclass pull-right addclas-on"><option value="0">Yes</option><option value="1">No</option></select></td><td class="show-text1"><span class="text1">보험사</span><select name="selecttext" class="form-control selectpickermember pull-right show-select1"><option value="0">심리적 증상</option><option value="1">심리적 증상</option></select></td><td><select name="selecttext" class="form-control selectpickermember pull-right"><option value="0">Yes</option><option value="1">No</option></select></td><td><select name="selecttext" class="form-control selectpickermember pull-right"><option value="0">심리적 증상</option><option value="1">심리적 증상</option></select></td></tr>');
-        $('.selectpickermember').selectpicker('refresh');
-         inputuploadimg(); 
-      });
-    }
-
-    $(window).on('load', function() {
-      addcolum();
     });
+    $("[data-bg-color], [data-bg-image], [data-bg-particles]").each(function() {
+        var $this = $(this);
 
-    // slide banner
-    function addpanner() {
-      $('#slide-banner').on('change', function(){
-        var minditem = $('#slide-banner').selectpicker('val');
-        var testimonials= $('.content-mind');
-       // $('.member-main-top').html('');
-          for (var i = 0; i < 5; i++) {
-            // Using $() to re-wrap the element.
-            // $(testimonials[i]).html('Testimonial by each loop');.html('Testimonial '+(index+1)+' by each loop');
-            if(i< minditem){
-              $(".slide-banner .main-content").removeClass('total-select0');
-              $(".slide-banner .main-content").removeClass('total-select1');
-              $(".slide-banner .main-content").removeClass('total-select2');
-              $(".slide-banner .main-content").removeClass('total-select3');
-              $(".slide-banner .main-content").addClass('total-select'+i);
-              $(testimonials[i]).html('<div class="main-left"><select name="selecttext" title="선택..." class="form-control selectpickermember text-left"><option data-hidden="true"></option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select></div><div class="main-right"><div class="item-upload"><div class="inputfile"><div class="tr-file-upload image-upload"><div class="preview-image"><img src="../../assets/images/no-img.png" alt="thumbnail"/><div class="content-input"><label class="btn green btn-select-file">찾아보기<input type="file" class="hidden input-file"/></label><div class="input-icon input-large input-inline right"><span class="show-file-info"></span></div></div></div></div></div></div></div>');
-              $('.selectpickermember').selectpicker('refresh');
-              inputuploadimg(); 
-            }else{
-               $(testimonials[i]).html('');
+        if( $this.hasClass("ts-separate-bg-element") ){
+            $this.append('<div class="ts-background">');
+
+            // Background Color
+
+            if( $("[data-bg-color]") ){
+                $this.find(".ts-background").css("background-color", $this.attr("data-bg-color") );
             }
-          }
-    
-      });
-    // var busiyear1 = $('#busiyear1').selectpicker('val');
-    // alert(busiyear1);
-    }
-    $(window).on('load', function() {
-      addpanner();
-    });
 
-    // intro page banner
-    function addintropage() {
-      $('#intro-page').on('change', function(){
-        var minditem = $('#intro-page').selectpicker('val');
-        var testimonials= $('.content-mind');
-       // $('.member-main-top').html('');
-          for (var i = 0; i < 5; i++) {
-            // Using $() to re-wrap the element.
-            // $(testimonials[i]).html('Testimonial by each loop');.html('Testimonial '+(index+1)+' by each loop');
-            if(i< minditem){
-            $(testimonials[i]).html('<div class="item-upload"><div class="inputfile"><div class="tr-file-upload image-upload"><div class="preview-image"><img src="../../assets/images/demo_single_2.jpg" alt="thumbnail"/></div><label class="btn green btn-select-file">찾아보기<input type="file" class="hidden input-file"/></label><div class="input-icon input-large input-inline right"><span class="show-file-info"></span></div></div></div></div>');
-            // $('.selectpickermember').selectpicker('refresh');
-            inputuploadimg(); 
-            }else{
-               $(testimonials[i]).html('');
+            // Particles
+
+            if( $this.attr("data-bg-particles-line-color") || $this.attr("data-bg-particles-dot-color") ){
+                $this.find(".ts-background").append('<div class="ts-background-particles">');
+                $(".ts-background-particles").each(function () {
+                    var lineColor = $this.attr("data-bg-particles-line-color");
+                    var dotColor = $this.attr("data-bg-particles-dot-color");
+                    var parallax = $this.attr("data-bg-particles-parallax");
+                    $(this).particleground({
+                        density: 15000,
+                        lineWidth: 0.2,
+                        lineColor: lineColor,
+                        dotColor: dotColor,
+                        parallax: parallax,
+                        proximity: 200
+                    });
+                });
             }
-          }
-    
-      });
-    // var busiyear1 = $('#busiyear1').selectpicker('val');
-    // alert(busiyear1);
-    }
-    $(window).on('load', function() {
-      addintropage();
-    });
 
-  })(jQuery);
+            // Background Image
+
+            if( $this.attr("data-bg-image") !== undefined ){
+                $this.find(".ts-background").append('<div class="ts-background-image">');
+                $this.find(".ts-background-image").css("background-image", "url("+ $this.attr("data-bg-image") +")" );
+                $this.find(".ts-background-image").css("background-size", $this.attr("data-bg-size") );
+                $this.find(".ts-background-image").css("background-position", $this.attr("data-bg-position") );
+                $this.find(".ts-background-image").css("opacity", $this.attr("data-bg-image-opacity") );
+
+                $this.find(".ts-background-image").css("background-size", $this.attr("data-bg-size") );
+                $this.find(".ts-background-image").css("background-repeat", $this.attr("data-bg-repeat") );
+                $this.find(".ts-background-image").css("background-position", $this.attr("data-bg-position") );
+                $this.find(".ts-background-image").css("background-blend-mode", $this.attr("data-bg-blend-mode") );
+            }
+
+            // Parallax effect
+
+            if( $this.attr("data-bg-parallax") !== undefined ){
+                $this.find(".ts-background-image").addClass("ts-parallax-element");
+            }
+        }
+        else {
+
+            if(  $this.attr("data-bg-color") !== undefined ){
+                $this.css("background-color", $this.attr("data-bg-color") );
+                if( $this.hasClass("btn") ) {
+                    $this.css("border-color", $this.attr("data-bg-color"));
+                }
+            }
+
+            if( $this.attr("data-bg-image") !== undefined ){
+                $this.css("background-image", "url("+ $this.attr("data-bg-image") +")" );
+
+                $this.css("background-size", $this.attr("data-bg-size") );
+                $this.css("background-repeat", $this.attr("data-bg-repeat") );
+                $this.css("background-position", $this.attr("data-bg-position") );
+                $this.css("background-blend-mode", $this.attr("data-bg-blend-mode") );
+            }
+
+        }
+    });
+})(jQuery);
 
   
-    
 
-
-  
-  (function($) {
-    $(document.body).on('change', '.selectpickeraddclass', function() {
-      var valoption = $('.selectpickeraddclass').selectpicker('val');
-      // alert(valoption);
-      if(valoption = 1){
-        $(".show-text1").addClass('show-select');
-        // alert(valoption);
-      }
-      else{
-        $("show-text1").removeClass('show-select');
-        // alert(valoption);
-      }
-    });
-   })(jQuery);
-
-   $(".checkall label input").click(function () {
-    if ($(this).is(":checked")){
-      var isAllChecked = 0;
-      $(".checkall label input").each(function(){
-        if(!this.checked)
-           isAllChecked = 1;
-      })              
-      if(isAllChecked == 0){ 
-        $(".checkall input#all").prop("checked", true); 
-        $.uniform.update('.checkall input#all');
-      }     
-    }else {
-      $(".checkall input#all").prop("checked", false);
-      $.uniform.update('.checkall input#all');
-    }
-  });
-
-
-   var status = $('.checkall input#all').is(':checked');
-   if(status == true){
-    $('.checkall label input').attr('checked', status);
-    $.uniform.update('.checkall label input');
-   }
-   else{
-    
-   }
    
-   $('.checkall input#all').bind('click', function(){
-      var status = $(this).is(':checked');
-      $('.checkall label input').attr('checked', status);
-      $.uniform.update('.checkall label input');
-    });
 
-   //paditech
-   $('#element_avia_2_1').on('click', function(){
-    
-      var status = $(this).is(':checked');
-      var status2 = $('#element_avia_3_1').is(':checked');
-      if(status2 == true){ 
-        alert(1);
-        $(this).prop('checked', false);
-      } 
-      else{
-        alert(2);
-        
-        $(this).prop('checked', true);
-      }
-      // $('.checkall label input').attr('checked', status);
-    });
-
-   $('#popupGiveRetrieveHelloCoin').bind('click', function(){
-      var status = $(this).is(':checked');
-      $('.checkall label input').attr('checked', status);
-      $.uniform.update('.checkall label input');
-    });
-
-   //tooltip
-   $(function () {
-      $('[data-toggle="tooltip"]').tooltip({html:true})
-    })
-
-  $('#checked-push input').change(function () {
-    if ($(this).is(":checked")) {
-        $("#checked-push").addClass('active');
-        $("#tab-none").addClass('active');
-    }
-    else {
-        $("#checked-push").removeClass('active');
-        $("#tab-none").removeClass('active');
-    };
-  });
-  
-
-});
-
-$(document.body).on('click', '.item-button .btn' , function(){
-  if($(this).hasClass('green')){
-      $(this).removeClass('green');
-      $(this).siblings().addClass('green');
-  }
-  else {
-      $(this).addClass('green');
-      $(this).siblings().removeClass('green');
-      }
-  });
-
-// jquery coupon popup
-$(document.body).on('click', '#send_coupon' , function(){
-  var couponnumber   = $(".coupon-number").text();
-  var couponnote  = $(".coupon-note").text();
-  $("#coupon-number").val(couponnumber);
-  $("#popup-coupon-note").text(couponnote);
-});
-
-// jquery chartjs
-
-// Data retrieved from http://vikjavev.no/ver/index.php?spenn=2d&sluttid=16.06.2015.
-
-
-
-// Build the chart
-if ($('#cos-pie-chart').length) {
-  Highcharts.chart('cos-pie-chart', {
-      chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false,
-          type: 'pie'
-      },
-      title: {
-          text: ''
-      },
-      tooltip: {
-          pointFormat: '{series.name}+ <b>{point.percentage:.1f}%</b>'
-      },
-      plotOptions: {
-          pie: {
-              allowPointSelect: true,
-              cursor: 'pointer',
-              dataLabels: {
-                enabled: false
-              },
-              showInLegend: true
-          }
-      },
-      series: [{
-          name: '',
-          // name: 'Brands',
-          colorByPoint: true,
-          data: [{
-              name: 'Cos A',
-              y: 140,
-              sliced: true,
-              selected: true
-          }, {
-              name: 'Cos B',
-              y: 10
-          }]
-      }]
-  });
-}
-// Build the chart
-  
 
 
 
